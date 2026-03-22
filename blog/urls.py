aa14168fb43d5c51b82cdba1f6622283b3700f1a
ngtr99 <minhnguyet12923@gmail.com>
2026-03-21 19:47:37 -0700

    change urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('posts/<int:id>', views.post_detail, name='post-detail'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
