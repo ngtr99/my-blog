f2c5f9535cc2cfeb201b5b4ec025e70688e3b2f8
ngtr99 <minhnguyet12923@gmail.com>
2026-03-21 19:38:01 -0700

    change description to TextField

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     location = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
