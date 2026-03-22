445fab99484fd647b63b487533b4de507a995c09
ngtr99 <minhnguyet12923@gmail.com>
2026-03-21 20:31:43 -0700

    replace image field with supabase image url

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Post(models.Model):
-    # models.py
-    image = models.URLField(max_length=200, blank=True, null=True)
     title = models.CharField(max_length=200)
     date = models.DateField()
     location = models.CharField(max_length=255)
     description = models.TextField()
+    image_url = models.URLField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.title
